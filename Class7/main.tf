6ae77594ce65afda639416db63fb50aba9e4ea1e
rmbkva <as.rmbkva@gmail.com>
2024-04-17 01:30:44 +0000

    added class7

---
+++ Class7/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "hello"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0900fe555666598a2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name               = aws_key_pair.deployer.key_name
+}
+
+#     connection {
+#     type     = "ssh"
+#     user     = "ec2-user"
+#     private_key = file("~/.ssh/id_rsa")
+#     host     = self.public_ip
+#   }
+
+# provisioner "file" {
+#     source = "./httpd.sh"        #local machine
+#     destination = "./httpd.sh"    #remote machine
+#     }
+
+
+
+#   provisioner "remote-exec" {
+#     script = "./httpd.sh"
+#     #   inline = [
+#     #     "sudo yum install httpd -y",
+#     #     "sudo systemctl start httpd",
+#     #     "sudo systemctl enable httpd"
+#     # ]
+#   }
+# }
+
+# resource "null_resource" "hello" {
+#      provisioner "local-exec" {
+#     command = "mkdir kaizen && touch hello"
+#   }
+# }
